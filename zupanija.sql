4480234d80b0e6b403c051754ec247ea02347300
Petar Jagrić <83279909+petarjagric@users.noreply.github.com>
2021-05-13 14:17:21 +0200

    altered tables

--- zupanija.sql
+++ zupanija.sql
@@ -1,5 +1,5 @@
 drop database if exists zupanija;
-create database if exists zupanija;
+create database zupanija;
 use zupanija;
 
 create table zupan(
@@ -25,3 +25,7 @@ sifra int not null primary key auto_increment,
 opcina int not null,
 naziv varchar(50)
 );
+
+alter table zupanija add foreign key (zupan) references zupan(sifra);
+alter table opcina add foreign key (zupanija) references zupanija(sifra);
+alter table mjesto add foreign key (opcina) references opcina(sifra);
