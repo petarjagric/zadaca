8d2fcfe6be88614a8d57a10eee98ce7117fe7995
Petar Jagrić <83279909+petarjagric@users.noreply.github.com>
2021-05-13 14:13:37 +0200

    created tables

--- zupanija.sql
+++ zupanija.sql
@@ -1,1 +1,27 @@
 drop database if exists zupanija;
+create database if exists zupanija;
+use zupanija;
+
+create table zupan(
+sifra int not null primary key auto_increment,
+ime varchar(50) not null,
+prezime varchar(50)
+);
+
+create table zupanija(
+sifra int not null primary key auto_increment,
+naziv varchar(50) not null,
+zupan int not null
+);
+
+create table opcina(
+sifra int not null primary key auto_increment,
+zupanija int not null,
+naziv varchar(50)
+);
+
+create table mjesto(
+sifra int not null primary key auto_increment,
+opcina int not null,
+naziv varchar(50)
+);
