a04f94baa98c987004492c825f0fb5d81b0f57d6
Petar Jagrić <83279909+petarjagric@users.noreply.github.com>
2021-05-14 15:30:54 +0200

    Update

--- zupanija.sql
+++ zupanija.sql
@@ -32,15 +32,34 @@ alter table mjesto add foreign key (opcina) references opcina(sifra);
 
 #1-3
 insert into zupan (ime,prezime) values
-('Ivan','Anušić'),
-('Božo','Galić'),
-('Danijel','Marušić');
+('Ivan','Anušić'),('Božo','Galić'),('Danijel','Marušić');
+select * from zupan;
 
 insert into zupanija(naziv,zupan) values
 ('Osiječko-Baranjska',1),('Vukovarsko-Srijemska',2),('Brodsko-Posavska',3);
+select * from zupanija;
 
 insert into opcina (zupanija,naziv) values
 (1,'Opićna Antunovac'),(1,'Općina Bizovac'),(2,'Općina Cerna'),(2,'Općina Borovo'),(3,'Općina Bebrina'),(3,'Općina Bukovlje');
+select * from opcina;
 
 insert into mjesto (opcina,naziv) values
-(1,'Antunovac'),(1,'Bizovac'),(1,'Bilje'),(1,'Darda'),(2,'Cerna'),(2,'Borovo'),(2,'Ivankovo'),(2,'Jarmina'),(3,'Bebrina'),(3,'Bukovlje'),(3,'Nova Gradiška'),(3,'Cernik')
+(1,'Antunovac'),(1,'Bizovac'),(1,'Bilje'),(1,'Darda'),(2,'Cerna'),(2,'Borovo'),(2,'Ivankovo'),(2,'Jarmina'),(3,'Bebrina'),(3,'Bukovlje'),(3,'Nova Gradiška'),(3,'Cernik');
+select * from mjesto;
+
+update mjesto
+set naziv = 'Čepin'
+where sifra=1;
+update mjesto
+set naziv = 'Petlovac'
+where sifra=2;
+update mjesto
+set naziv = 'Podgoroač'
+where sifra=3;
+update mjesto
+set naziv = 'Andrijaševci'
+where sifra=5;
+update mjesto
+set naziv = 'Rokovci'
+where sifra=6;
+
